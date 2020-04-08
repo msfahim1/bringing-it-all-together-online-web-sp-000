2c70499227de5e6eff3cb28e0b68d03b9bb7222a
Michael Fahim <michael_fahim@yahoo.com>
2020-04-08 00:00:33 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+require 'pry'
+
+describe "Dog" do
+
+  let(:teddy) {Dog.new(name: "Teddy", breed: "cockapoo")}
+
+  before(:each) do
+    DB[:conn].execute("DROP TABLE IF EXISTS dogs")
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+        )
+    SQL
+    DB[:conn].execute(sql)
+  end
